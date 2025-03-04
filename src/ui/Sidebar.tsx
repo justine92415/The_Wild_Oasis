d2161cf390a9651f3676b2e3e25e0c3717da2cd7
justine92415 <justine92415@gmail.com>
2025-03-04 21:28:37 +0800

    feat: 調整 Logo、MainNav、SideBar 元件樣式

--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,7 +1,14 @@
+import Logo from "./Logo";
+import MainNav from "./MainNav";
+
 function Sidebar() {
   return (
-    <aside className="border-grey-100 bg-grey-0 row-span-full border-r border-solid px-6 py-8">
-      SIDEBAR
+    <aside
+      className="border-grey-100 bg-grey-0 row-span-full flex flex-col gap-8 border-r
+        border-solid px-6 py-8"
+    >
+      <Logo />
+      <MainNav />
     </aside>
   );
 }
