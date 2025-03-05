ac72c588f072b1396a5196dafcc3b62a751f5f1a
justine92415 <justine92415@gmail.com>
2025-03-05 20:36:39 +0800

    feat:347.新增 React Query 支援以改善資料獲取管理

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+    },
+  },
+});
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
