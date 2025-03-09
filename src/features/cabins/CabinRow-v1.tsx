75c6f8dd11899d306153cd5492757c589bbf7eca
justine92415 <justine92415@gmail.com>
2025-03-09 16:41:44 +0800

    feat(cabins):373.整合小屋行元件的操作選單，提升使用者互動性

--- src/features/cabins/CabinRow-v1.tsx
+++ src/features/cabins/CabinRow-v1.tsx
@@ -7,7 +7,6 @@ import { useCreateCabin } from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import Table from "../../ui/Table";
-import Menus from "../../ui/Menus";
 
 function CabinRow({ cabin }: { cabin: Cabin }) {
   const { isCreating, createCabin } = useCreateCabin();
@@ -61,31 +60,27 @@ function CabinRow({ cabin }: { cabin: Cabin }) {
       )}
 
       <div>
+        <button onClick={() => handleDuplicate()} disabled={isCreating}>
+          <HiSquare2Stack />
+        </button>
+
         <Modal>
-          <Menus.Menu>
-            <Menus.Toggle id={cabinId} />
-            <Menus.List id={cabinId}>
-              <Menus.Button
-                onClick={() => handleDuplicate()}
-                icon={<HiSquare2Stack />}
-              >
-                Duplicate
-              </Menus.Button>
-
-              <Modal.Open opens="edit">
-                <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
-              </Modal.Open>
-
-              <Modal.Open opens="delete">
-                <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
-              </Modal.Open>
-            </Menus.List>
-          </Menus.Menu>
-
+          <Modal.Open opens="edit">
+            <button>
+              <HiPencil />
+            </button>
+          </Modal.Open>
           <Modal.Window name="edit">
             <CreateCabinForm cabinToEdit={cabin} />
           </Modal.Window>
+        </Modal>
 
+        <Modal>
+          <Modal.Open opens="delete">
+            <button>
+              <HiTrash />
+            </button>
+          </Modal.Open>
           <Modal.Window name="delete">
             <ConfirmDelete
               resourceName="cabins"
